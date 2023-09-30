8c890e190c24e48220128ca4ed640ca36905f290
Marc Guiselin <24906387+MarcGuiselin@users.noreply.github.com>
2023-09-30 14:08:14 +0200

    remove

--- crates/launcher/src/invoke.rs
+++ crates/launcher/src/invoke.rs
@@ -1,6 +1,3 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
 use crate::{state, task::*};
 
 type State<'r> = tauri::State<'r, state::State>;
